af227de21a0b4b5bc5f0c8d2d19d76e3297d3f31
Melvin Lammerts <hi@melvin.sh>
2016-08-25 19:55:45 +0200

    Create gem with executable

--- Gemfile
+++ Gemfile
@@ -2,6 +2,4 @@ source 'https://rubygems.org'
 
 gem 'pry'
 
-gem 'tty'
-gem 'http'
-gem 'nokogiri'
+gemspec
--- test/test.sh
+++ test/test.sh
@@ -1,35 +1,35 @@
 # VERY basic tests just to see if nothing breaks easily.
 
 echo "[Without command]"
-ruby vcsmap.rb
+bin/vcsmap
 
 echo "[With list command]"
-ruby vcsmap.rb list
+bin/vcsmap list
 
 echo "[With invalid command]"
-ruby vcsmap.rb invalid
+bin/vcsmap invalid
 
 echo "[With run command, no plugin]"
-ruby vcsmap.rb run
+bin/vcsmap run
 
 echo "[With run command, invalid plugin]"
-ruby vcsmap.rb run invalid
+bin/vcsmap run invalid
 
 echo "[With run command, valid plugin, invalid pages]"
-ruby vcsmap.rb run filezilla_xml invalid
+bin/vcsmap run filezilla_xml invalid
 
 echo "[With run command, valid plugin, invalid pages, no ascii]"
-ruby vcsmap.rb run filezilla_xml invalid --no-ascii
+bin/vcsmap run filezilla_xml invalid --no-ascii
 
 echo "[With run command, valid plugins, 1 page]"
-ruby vcsmap.rb run filezilla_xml 1
-ruby vcsmap.rb run google_oauth 1
-ruby vcsmap.rb run solr_dataconfig 1
-ruby vcsmap.rb run sublime_github 1
-ruby vcsmap.rb run wordpress_config 1
+bin/vcsmap run filezilla_xml 1
+bin/vcsmap run google_oauth 1
+bin/vcsmap run solr_dataconfig 1
+bin/vcsmap run sublime_github 1
+bin/vcsmap run wordpress_config 1
 
 echo "[With run command, valid plugin, 2 pages]"
-ruby vcsmap.rb run filezilla_xml 2
+bin/vcsmap run filezilla_xml 2
 
 echo "[With run command, valid plugin, 1 page, no ascii]"
-ruby vcsmap.rb run filezilla_xml 1 --no-ascii
+bin/vcsmap run filezilla_xml 1 --no-ascii
